005dd1cd48c30f518adaad4a75a5d33660abf9f4
S.P. Mohanty <spmohanty@pb-d-128-141-253-193.cern.ch>
2014-10-30 20:00:36 +0100

    Adding support for crawling multiple pages of the same timeline

--- collect.py
+++ collect.py
@@ -1,16 +1,34 @@
 import urllib2
 from bs4 import BeautifulSoup
+from datetime import datetime
+import json
 
-page = urllib2.urlopen('http://timeline.web.cern.ch/timelines/Building-the-LHC').read()
-soup = BeautifulSoup(page)
+items = []
 
-def innerHTML(element):
-    return element.decode_contents(formatter="html")
+for pageNum in range(100):
+	page = urllib2.urlopen('http://timeline.web.cern.ch/timelines/Building-the-LHC?page='+str(pageNum)).read()
+	soup = BeautifulSoup(page)
 
-for article in soup.findAll('article'):
-	# print dir(article)
-	print "="*10
-	print article.findChildren("h2")[0].findChildren("a")[0].text #Heading
-	print article.findChildren("span")[0].text #Date
-	print article.findChildren("img") #Image tag
-	print innerHTML(article.findChildren("div", {"class":"field-item"})[0])
+	def innerHTML(element):
+	    return element.decode_contents(formatter="html")
+	count = 0
+	for article in soup.findAll('article'):
+		count += 1
+		# print dir(article)
+		d = {}
+		d['headline'] = article.findChildren("h2")[0].findChildren("a")[0].decode_contents(formatter="html")
+		d['icon'] = 'https://upload.wikimedia.org/wikipedia/commons/c/cc/Ambox_green_tick.png'
+		d['description'] = innerHTML(article.findChildren("div", {"class":"field-item"})[0])
+		d['date'] = article.findChildren("span")[0].text #Date
+
+		date_object = datetime.strptime(article.findChildren("span")[0].text, '%d %B %Y')
+		d['date'] = str(date_object.year)+","+str(date_object.month)+","+str(date_object.day)
+		images=article.findChildren("img") #Image tag
+		if len(images)>0:
+			d['media'] = images[0]['src']
+			d['icon'] = images[0]['src']
+		items.append(d)
+	if count == 0:
+		break
+
+print json.dumps(items)
